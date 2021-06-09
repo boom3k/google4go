812d2a0e094a8333d47ddf59b85f8875be5bab22
Ramel Henderson <boom3k@users.noreply.github.com>
2021-06-09 00:30:45 -0400

    Added static scopes

--- client.go
+++ client.go
@@ -13,6 +13,40 @@ import (
 	"os"
 )
 
+
+var AdminScopes = []string{
+	"https://www.googleapis.com/auth/admin.reports.audit.readonly",
+	"https://www.googleapis.com/auth/admin.reports.usage.readonly",
+	"https://www.googleapis.com/auth/admin.directory.user",
+	"https://www.googleapis.com/auth/admin.directory.group.member",
+	"https://www.googleapis.com/auth/admin.directory.group",
+	"https://www.googleapis.com/auth/admin.directory.customer",
+	"https://www.googleapis.com/auth/admin.directory.resource.calendar",
+	"https://www.googleapis.com/auth/admin.directory.domain",
+	"https://www.googleapis.com/auth/apps.groups.settings",
+	"https://www.googleapis.com/auth/androidmanagement",
+	"https://www.googleapis.com/auth/apps.groups.migration",
+	"https://www.googleapis.com/auth/apps.groups.settings",
+	"https://www.googleapis.com/auth/admin.datatransfer",
+	"https://www.googleapis.com/auth/cloud-platform",
+	"https://www.googleapis.com/auth/cloudplatformprojects",
+	"https://www.googleapis.com/auth/cloud_search",
+	"https://www.googleapis.com/auth/apps.licensing",
+	"https://www.googleapis.com/auth/admin.directory.device.mobile"}
+var ServiceAccountScopes = []string{
+	"https://mail.google.com/",
+	"https://sites.google.com/feeds",
+	"https://www.google.com/m8/feeds",
+	"https://www.googleapis.com/auth/drive",
+	"https://www.googleapis.com/auth/activity",
+	"https://www.googleapis.com/auth/calendar",
+	"https://www.googleapis.com/auth/contacts",
+	"https://www.googleapis.com/auth/userinfo.email",
+	"https://www.googleapis.com/auth/userinfo.profile",
+	"https://www.googleapis.com/auth/gmail.settings.basic",
+	"https://www.googleapis.com/auth/gmail.settings.sharing",
+}
+
 var globalClient = &http.Client{}
 
 // InitializeOauth2Client Used for Oauth2 authorized clients
